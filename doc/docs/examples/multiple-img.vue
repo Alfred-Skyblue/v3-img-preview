5f831d72cb0f5604cccb896941f1e530f2ec8ac4
yangpanteng <893042011@qq.com>
2022-03-23 21:01:57 +0800

    docs：写文档

--- doc/docs/examples/multiple-img.vue
+++ doc/docs/examples/multiple-img.vue
@@ -13,7 +13,8 @@
 
 <script lang="ts" setup>
 import { v3ImgPreviewFn } from 'v3-img-preview'
-const images = [1, 2, 3, 4, 5].map(i => `/images/img${i}.jpg`)
+
+const images = [1, 2, 3, 4, 5].map(i => `/v3-img-preview/images/img${i}.jpg`)
 const handlePreview = (index: number) => {
   console.log('-> index', index)
   v3ImgPreviewFn({ images, index })
