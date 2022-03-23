e197636eddf5da05b733e0af99adf8efbb1156ab
yangpanteng <893042011@qq.com>
2022-03-23 22:09:04 +0800

    docs：写文档

--- doc/docs/examples/single-img.vue
+++ doc/docs/examples/single-img.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <img
-      src="/images/img1.jpg"
-      alt=""
-      @click.stop="handlePreview('/images/img1.jpg')"
-    />
+    <img src="/images/img1.jpg" alt="" @click.stop="handlePreview" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { v3ImgPreviewFn } from 'v3-img-preview'
-const handlePreview = (src: string) => v3ImgPreviewFn(src)
+const handlePreview = (e: MouseEvent) => {
+  console.log('-> e', e)
+  v3ImgPreviewFn((e.target as HTMLImageElement).src)
+}
 </script>
