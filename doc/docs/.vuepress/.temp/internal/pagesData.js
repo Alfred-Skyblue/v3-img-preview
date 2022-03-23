f63f7b62c60fc1a9b369673ccf010e6696202287
yangpanteng <893042011@qq.com>
2022-03-23 21:04:05 +0800

    docs：写文档

--- doc/docs/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,4 +0,0 @@
-export const pagesData = {
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Volumes/office/code/Plugins/v3-img-preview/doc/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
--- doc/docs/.vuepress/.temp/register-components/clientAppEnhance.4fd80027.js
+++
@@ -1,6 +0,0 @@
-    import { defineAsyncComponent } from 'vue'
-    
-    export default ({ app }) => {    
-      app.component("DemoBlock", defineAsyncComponent(() => import("/Volumes/office/code/Plugins/v3-img-preview/doc/node_modules/vuepress-plugin-demo-block-vue3/src/DemoBlock.vue")))
-    }
-    
