207c49b9c341b968d5dd45d1b23f46811dbb7e32
yangpanteng <893042011@qq.com>
2022-03-23 20:21:16 +0800

    docs：写文档

--- doc/assets/link-src.f532480a.js
+++
@@ -1,1 +0,0 @@
-import{d as o,o as a,c,y as n}from"./app.7cb6c2a7.js";const i=o({setup(s){const t="https://img0.baidu.com/it/u=3155150237,2943070590&fm=253&fmt=auto&app=138&f=JPEG?w=667&h=500",e=()=>{n(t)};return(r,m)=>(a(),c("img",{src:t,alt:"",onClick:e}))}});export{i as default};
