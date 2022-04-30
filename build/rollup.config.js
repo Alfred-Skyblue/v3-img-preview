51ab78026812ba61e063a5bdaf75f775a47b9d69
yangpanteng <893042011@qq.com>
2022-04-30 14:48:29 +0800

    build: 修改打包配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -68,8 +68,6 @@ const baseConfig = {
   }
 }
 
-// ESM/UMD/IIFE shared settings: externals
-// Refer to https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
 const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
