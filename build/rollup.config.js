07e93d19255b47164ce879a336c79f72ec3cf6c5
丶远方 <yangpanteng@gmail.com>
2022-05-19 19:29:56 +0800

    build: 修改打包配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -70,8 +70,6 @@ const baseConfig = {
 
 const external = ['vue']
 
-// UMD/IIFE shared settings: output.globals
-// Refer to https://rollupjs.org/guide/en#output-globals for details
 const globals = {
   // Provide global variable names to replace your external imports
   // eg. jquery: '$'
